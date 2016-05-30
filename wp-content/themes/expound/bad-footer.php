c481f24e52fd96b1d107668a72b9e7a63b9be981
andytan2624 <andy.tan2624@gmail.com>
2016-05-30 16:08:22 +1000

    Changing files to include new changes

---
+++ wp-content/themes/expound/bad-footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ash_skywalker10
+ * Date: 22/05/2016
+ * Time: 6:52 PM
+ */
---
+++ wp-content/themes/expound/clearance.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ash_skywalker10
+ * Date: 29/05/2016
+ * Time: 12:43 PM
+ */
